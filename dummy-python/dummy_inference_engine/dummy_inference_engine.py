7cff97cf495e4343e729073d5c384acf32c42ebd
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-04-06 16:16:21 +0200

    set up dummies

--- dummy-python/dummy_inference_engine/dummy_inference_engine.py
+++ dummy-python/dummy_inference_engine/dummy_inference_engine.py
@@ -1,7 +1,8 @@
+import snappy
+
 class dummy_inference_engine:
 
-    def infer(self, coarse_res_brdf_descriptors, high_res_sdr, grd_sar_data, prior, optical_forward_operator_emulator,
+    def infer(self, coarse_res_brdf_descriptor, high_res_sdr, grd_sar_data, prior, optical_forward_operator_emulator,
               sar_forward_operator_emulator):
-        print("The inference engine infers high resolution biophysical parameters.")
-        high_res_biophysical_parameters = []
-        return high_res_biophysical_parameters
+        print ('Inferring high resolution biophysical parameters')
+        return snappy.Product('high_res_biophysical_params', 'high_res_biophysical_params', 1, 1)
