b9d794134c8a31b58dfb056bc82542b633a8800d
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-02-02 18:29:58 +0100

    added new inference priors

--- multiply_inference_engine/inference_prior.py
+++ multiply_inference_engine/inference_prior.py
@@ -1,9 +1,12 @@
 from abc import ABCMeta, abstractmethod
 from datetime import datetime
+from multiply_core.util import Reproject
 from multiply_prior_engine import PriorEngine
 from typing import List, Union
+import gdal
 import logging
 import numpy as np
+import os
 
 __author__ = "Tonio Fincke (Brockmann Consult GmbH)"
 
@@ -13,12 +16,14 @@ LOG = logging.getLogger(__name__ + ".inference_prior")
 class InferencePrior(object):
     """A class to wrap access to priors created by the MULTIPLY prior engine to the inference engine."""
 
-    def __init__(self, prior_engine_config_file: str, global_prior_files: List[str], use_dummy: bool=False):
+    def __init__(self, prior_engine_config_file: str, global_prior_files: List[str], reference_dataset: gdal.Dataset,
+                 use_dummy: bool=False):
         """
         This class encapsulates the access to priors produced by the MULTIPLY Prior Engine by either encapsulating
         the whole Prior Engine or by retrieving a number of global prior files that were the output of the Prior Engine.
         :param prior_engine_config_file: A YAML config file to set up the prior engine
         :param global_prior_files: A list of files that contain information on priors. Files must named according to
+        :param reference_dataset: A data set with the spatial extent and resolution to which the prior shall be mapped
         format: 'Priors_<name of parameter>_<name of another parameter>_<day of year>_global.vrt.
         """
         if use_dummy:
@@ -26,11 +31,11 @@ class InferencePrior(object):
             self._inference_prior = DummyInferencePrior()
         elif global_prior_files is not None:
             LOG.info('Using global files to access prior information')
-            self._inference_prior = PriorFilesInferencePrior(global_prior_files)
+            self._inference_prior = PriorFilesInferencePrior(global_prior_files, reference_dataset)
             if prior_engine_config_file is not None and prior_engine_config_file is not '':
                 LOG.info('Passing a config file is not necessary when prior files are present')
         elif prior_engine_config_file is not None and prior_engine_config_file is not '':
-            self._inference_prior = PriorEngineInferencePrior(prior_engine_config_file)
+            self._inference_prior = PriorEngineInferencePrior(prior_engine_config_file, reference_dataset)
         else:
             raise ValueError('Either config for prior engine or list of vrt files must be given as parameter.')
             # This method sets up the MULTIPLY prior engine using a YAML (e.g.)
@@ -72,7 +77,7 @@ class _WrappingInferencePrior(metaclass=ABCMeta):
                       inv_cov: bool = True) -> List[np.array]:
         """
         This method retrieves the requested parameters for the given time and region. For each parameter, it returns
-        a mean state vector and sparse covariance matrix which might be sparse.
+        a mean state vector and covariance matrix which might be inverse.
         :param parameters: List of parameters for which prior information is requested
         :param time: The time for which information is requested.
         :param state_grid: A state grid to indicate the region for which information must be retrieved.
@@ -88,7 +93,7 @@ class _WrappingInferencePrior(metaclass=ABCMeta):
 
 class PriorEngineInferencePrior(_WrappingInferencePrior):
 
-    def __init__(self, prior_engine_config_file: str):
+    def __init__(self, prior_engine_config_file: str, reference_dataset: gdal.Dataset):
         """
         This class encapsulates the access to priors produced by the MULTIPLY Prior Engine by either encapsulating
         the whole Prior Engine or by retrieving a number of global prior files that were the output of the Prior Engine.
@@ -96,29 +101,73 @@ class PriorEngineInferencePrior(_WrappingInferencePrior):
         format: 'Priors_<name of parameter>_<name of another parameter>_<day of year>_global.vrt.
         """
         self._prior_engine_config_file = prior_engine_config_file
+        self._reference_dataset = reference_dataset
 
     def process_prior(self, parameters: List[str], time: Union[str, datetime], state_grid: np.array,
                       inv_cov: bool = True) -> List[np.array]:
         if type(time) is datetime:
-            time = datetime.strptime(time, 'yyyy-mm-dd')
+            time = datetime.strftime(time, "%Y-%m-%d")
         prior_engine = PriorEngine(datestr=time, variables=parameters, config=self._prior_engine_config_file)
         priors = prior_engine.get_priors()
-
+        num_pixels = state_grid.sum()
+        num_params = len(parameters)
+        shape = num_pixels * num_params
+        processed_priors = []
+        mean_state_vector = np.empty(shape=shape, dtype=np.float32)
+        covariance_vector = np.empty(shape=shape, dtype=np.float32)
+        for i, parameter in enumerate(parameters):
+            vrt_dataset = list(priors[parameter].values())[0]
+            reprojected_vrt_dataset = Reproject.reproject_image(vrt_dataset, self._reference_dataset)
+            mean_state_vector[i::num_params] = reprojected_vrt_dataset.GetRasterBand(1).ReadAsArray()[state_grid]
+            covariance_vector[i::num_params] = reprojected_vrt_dataset.GetRasterBand(2).ReadAsArray()[state_grid]
+        processed_priors.append(mean_state_vector)
+        processed_priors.append(covariance_vector)
+        return processed_priors
 
 class PriorFilesInferencePrior(_WrappingInferencePrior):
 
-    def __init__(self, global_prior_files: List[str]):
+    def __init__(self, global_prior_file_paths: List[str], reference_dataset: gdal.Dataset):
         """
         This class encapsulates the access to priors produced by the MULTIPLY Prior Engine by retrieving a number of
         global prior files that were the output of the Prior Engine.
-        :param global_prior_files: A list of files that contain information on priors. Files must named according to
-        format: 'Priors_<name of parameter>_<name of another parameter>_<day of year>_global.vrt.
+        :param global_prior_file_paths: A list of absolute paths to files that contain information on priors. Files must be
+        named according to format: 'Priors_<name of parameter>_<day of year>_global.vrt.
         """
-        self._global_prior_files = global_prior_files
+        self._global_prior_file_paths = global_prior_file_paths
+        self._global_prior_file_names = []
+        for path in self._global_prior_file_paths:
+            file = open(path)
+            self._global_prior_file_names.append(os.path.basename(file.name))
+        self._reference_dataset = reference_dataset
 
     def process_prior(self, parameters: List[str], time: Union[str, datetime], state_grid: np.array,
                       inv_cov: bool = True) -> List[np.array]:
-        pass
+        num_pixels = state_grid.sum()
+        num_params = len(parameters)
+        shape = num_pixels * num_params
+        processed_priors = []
+        mean_state_vector = np.empty(shape=shape, dtype=np.float32)
+        covariance_vector = np.empty(shape=shape, dtype=np.float32)
+        if type(time) is str:
+            time = datetime.strptime(time, "%Y-%m-%d")
+        day_of_year = time.timetuple().tm_yday
+        month = time.month
+        for i, parameter in enumerate(parameters):
+            if parameter == 'sm':
+                requested_prior_file_name = '{}_prior_climatology_{:02d}.vrt'.format(parameter, month)
+            else:
+                requested_prior_file_name = 'Priors_{}_{:03d}_global.vrt'.format(parameter, day_of_year)
+            indices = [j for j, global_prior_file_name in enumerate(self._global_prior_file_names)
+                if global_prior_file_name == requested_prior_file_name]
+            if len(indices) == 0:
+                raise UserWarning('Could not find prior file {}.'.format(requested_prior_file_name))
+            vrt_dataset = gdal.Open(self._global_prior_file_paths[indices[0]])
+            reprojected_vrt_dataset = Reproject.reproject_image(vrt_dataset, self._reference_dataset)
+            mean_state_vector[i::num_params] = reprojected_vrt_dataset.GetRasterBand(1).ReadAsArray()[state_grid]
+            covariance_vector[i::num_params] = reprojected_vrt_dataset.GetRasterBand(2).ReadAsArray()[state_grid]
+        processed_priors.append(mean_state_vector)
+        processed_priors.append(covariance_vector)
+        return processed_priors
 
 
 class DummyInferencePrior(_WrappingInferencePrior):
@@ -133,7 +182,7 @@ class DummyInferencePrior(_WrappingInferencePrior):
         shape = num_pixels * num_params
         processed_priors = []
         mean_state_vector = np.empty(shape=shape, dtype=np.float32)
-        inverse_covariance_matrix = np.empty(shape=[shape, shape], dtype=np.float32)
+        inverse_covariance_matrix = np.empty(shape=shape, dtype=np.float32)
         processed_priors.append(mean_state_vector)
         processed_priors.append(inverse_covariance_matrix)
         return processed_priors
--- test/inference_engine/inference_prior_test.py
+++ test/inference_engine/inference_prior_test.py
@@ -1,10 +1,16 @@
+import gdal
 import numpy as np
 from multiply_inference_engine import InferencePrior
 
-GLOBAL_VRT_FILE = './test_data/Priors_lai_060_global.vrt'
+GLOBAL_LAI_VRT_FILE = './test/test_data/Priors/Priors_lai_060_global.vrt'
+GLOBAL_CAB_VRT_FILE = './test/test_data/Priors/Priors_cab_060_global.vrt'
+GLOBAL_SM_VRT_FILE = './test/test_data/sm_prior_climatology_03.vrt'
+REFERENCE_FILE = './test/test_data/Priors/T32UME_20170910T104021_B10.tiff'
+PRIOR_CONFIG_FILE = './test/test_data/Priors/prior_engine_test_config.yml'
+
 
 def test_process_dummy_prior():
-    inference_prior = InferencePrior('', [], True)
+    inference_prior = InferencePrior('', [], None, True)
     parameters = ['cogito', 'ergo', 'sum']
     state_grid = np.zeros(shape=[20, 10], dtype=np.bool)
     state_grid[1:4, 1:4] = True
@@ -12,18 +18,96 @@ def test_process_dummy_prior():
     processed_priors = inference_prior.process_prior(parameters, 'i actually do not matter for this test',
                                                      state_grid, True)
     assert type(processed_priors) is list
-    assert 2, len(processed_priors)
-    assert 54, processed_priors[0].shape
-    assert [54, 54], processed_priors[1].shape
+    assert 2 == len(processed_priors)
+    assert 54 == len(processed_priors[0])
+    assert 54 == len(processed_priors[1])
 
 
 def test_process_vrt_prior():
-    inference_prior = InferencePrior('', [GLOBAL_VRT_FILE])
-    parameters = ['lai']
-    state_grid = np.zeros(shape=[20, 10], dtype=np.bool)
-    state_grid[1:4, 1:4] = True
-    state_grid[13:16, 5:8] = True
+    reference_data_set = gdal.Open(REFERENCE_FILE)
+    inference_prior = InferencePrior('', [GLOBAL_LAI_VRT_FILE, GLOBAL_SM_VRT_FILE, GLOBAL_CAB_VRT_FILE],
+                                     reference_data_set)
+    parameters = ['lai', 'cab', 'sm']
+    state_grid = np.zeros(shape=[10980, 1830], dtype=np.bool8)
+    state_grid[1001:1003, 1701:1703] = True
+    state_grid[10001:10004, 1001:1004] = True
     processed_priors = inference_prior.process_prior(parameters, '2017-03-01', state_grid, True)
 
     assert type(processed_priors) is list
-    assert 2, len(processed_priors)
+    assert 2 == len(processed_priors)
+    assert 39 == len(processed_priors[0])
+    expected_state_mean_vectors = np.array([0.78757334, 0.99484605, 0.220177,
+                                            0.78757334, 0.99484605, 0.220177,
+                                            0.78757334, 0.99484605, 0.220177,
+                                            0.78757334, 0.99484605, 0.220177,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518]
+                                           , dtype=np.float32)
+    np.testing.assert_array_almost_equal(expected_state_mean_vectors, processed_priors[0])
+    assert 39 == len(processed_priors[1])
+    expected_covariance_vectors = np.array([0.1117579, 0.00289027, 0.0221391,
+                                            0.1117579, 0.00289027, 0.0221391,
+                                            0.1117579, 0.00289027, 0.0221391,
+                                            0.1117579, 0.00289027, 0.0221391,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105]
+                                           )
+    np.testing.assert_array_almost_equal(expected_covariance_vectors, processed_priors[1])
+
+def test_process_prior_engine_prior():
+    reference_data_set = gdal.Open(REFERENCE_FILE)
+    inference_prior = InferencePrior(PRIOR_CONFIG_FILE, None, reference_data_set)
+    parameters = ['lai', 'cab', 'sm']
+    state_grid = np.zeros(shape=[10980, 1830], dtype=np.bool8)
+    state_grid[1001:1003, 1701:1703] = True
+    state_grid[10001:10004, 1001:1004] = True
+    processed_priors = inference_prior.process_prior(parameters, '2017-03-01', state_grid, True)
+
+    assert type(processed_priors) is list
+    assert 2 == len(processed_priors)
+    assert 39 == len(processed_priors[0])
+    expected_state_mean_vectors = np.array([0.78757334, 0.99484605, 0.220177,
+                                            0.78757334, 0.99484605, 0.220177,
+                                            0.78757334, 0.99484605, 0.220177,
+                                            0.78757334, 0.99484605, 0.220177,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518,
+                                            0.47024861, 0.59706515, 0.264518]
+                                           , dtype=np.float32)
+    np.testing.assert_array_almost_equal(expected_state_mean_vectors, processed_priors[0])
+    assert 39 == len(processed_priors[1])
+    expected_covariance_vectors = np.array([0.1117579, 0.00289027, 0.0221391,
+                                            0.1117579, 0.00289027, 0.0221391,
+                                            0.1117579, 0.00289027, 0.0221391,
+                                            0.1117579, 0.00289027, 0.0221391,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105,
+                                            0.06780638, 0.00169024, 0.0213105]
+                                           )
+    np.testing.assert_array_almost_equal(expected_covariance_vectors, processed_priors[1])
