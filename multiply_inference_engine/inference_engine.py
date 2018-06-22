316b1f6cdc6f63b9755e18487dc68d830b863f15
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-06-22 12:03:36 +0200

    fix

--- multiply_inference_engine/inference_engine.py
+++ multiply_inference_engine/inference_engine.py
@@ -148,7 +148,7 @@ def infer(start_time: Union[str, datetime],
     linear_kalman.set_trajectory_uncertainty(Q)
 
     time_grid = [start_time, end_time]
-    linear_kalman.run(time_grid, x_forecast, None, p_forecast_inv, iter_obs_op==True)
+    linear_kalman.run(time_grid, x_forecast, None, p_forecast_inv, iter_obs_op=True)
 
 
 if __name__ == '__main__':
